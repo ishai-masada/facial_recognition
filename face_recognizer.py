43947d53a6e0076e84f416072af2882214f4edee
root <root@AEIM.localdomain>
2023-01-01 14:23:10 -0500

    heh

--- face_recognizer.py
+++ face_recognizer.py
@@ -11,12 +11,19 @@ cascade_path = os.path.dirname(cv2.__file__) + "/data/haarcascade_frontalface_de
 cascade = cv2.CascadeClassifier(cascade_path)
 
 # Load Camera
-vid = cv2.VideoCapture(0)
+#vid = cv2.VideoCapture(0)
 
-if not vid.isOpened():
-    print("Cannot open camera")
+#if not vid.isOpened():
+#    print("Cannot open camera")
 
 #while True:
 #    ret, frame = capture.read()
     
 #    key = cv2.waitKey(1)
+
+gray = cv2.cvtColor(img_1, cv2.COLOR_BGR2GRAY)
+print(len(gray))
+
+#face = cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30,30), flags=cv2.CASCADE_SCALE_IMAGE)
+
+#print(face)
