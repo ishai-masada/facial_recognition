f7fb3f852d8e37b85a74c096056ed6f7a6050033
root <root@AEIM.localdomain>
2023-01-03 15:02:57 -0500

    added comments and made a directory for videos

--- face_recognizer.py
+++ face_recognizer.py
@@ -1,6 +1,8 @@
 import cv2
 
-vid_path = 'IMG_5094.MOV'
+vid_path = 'videos/IMG_5094.MOV'
+
+# Load the cascade from OpenCV
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 # Load Camera Input
@@ -9,15 +11,26 @@ vid = cv2.VideoCapture(vid_path)
 #print(vid.isOpened())
 
 while True:
+    # Input the frame from the video
     _, frame = vid.read()
+
+    # Convert it from color to grayscale
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Find the face in the frame
     face = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30,30), flags=cv2.CASCADE_SCALE_IMAGE)
+
+    # Draw a rectangle around the face
     for (x, y, w, h) in face:
         cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 2)
+
+    # Display the frame
+    cv2.namedWindow('video', cv2.WINDOW_NORMAL)
+    cv2.resizeWindow('video', 500, 500)
     cv2.imshow('video', frame)
     
+    # Don't know what this is yet
     key = cv2.waitKey(1)
-
     if key == 27 and 0xff == ord('q'):
         break
 
